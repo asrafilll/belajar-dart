8b67e6f5349293013fd478a8f36b1b4d68735d5e
asrafilll <asrafilr@gmail.com>
2021-10-24 13:51:51 +0700

    initial commit

---
+++ challenge1.dart
@@ -0,0 +1,28 @@
+void main() {
+
+  /* 
+  Challenge 1
+  1. Buatlah variabel dari data dibawah sesuai dengan tipe datanya!
+  2. Buatlah sebuah map yang berisikan data dibawah menggunakan variabel yang telah dibuat!
+  3. Print map tersebut!
+
+
+  Data Restoran
+  -------------
+  Nama: Rifqi Seafood
+  Tahun Didirikan: 2000
+  Pemilik: Riqi Eka Hardianto
+  Alamat: Jl. Bhayangkara, Jakarta
+  Telepon: 08123456789
+  Status Buka: Buka (Buka/Tutup)
+  Daftar Makanan: 
+    - Kepiting Rebus (40rb)
+    - Nasi Goreng (20rb)
+    - Udang Asam Manis (50rb)
+    - Sate Cumi (30rb)
+  Daftar Minuman: 
+  
+
+
+
+}
---
+++ first_program.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nama = 'Asrafil Rizal';
+  int umur = 12;
+  String daftarHewan = "Kucing, Anjing, Kuda";
+  // String usia = umur.toString();
+
+  // var daftarHewanBaru = daftarHewan.split(', ');
+  // print(daftarHewanBaru);
+
+  print(nama.indexOf('s'));
+}
